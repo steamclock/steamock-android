38d353eaf7212e47c9c320b69bae9f07940708be
Shayla Sawchenko <shayla@steamclock.com>
2024-01-19 15:33:42 -0800

    Use x-mock-response-id so that admin does not have to make each mock have a unique path

--- lib-core/src/main/java/com/steamclock/steamock/lib/repo/MockResponse.kt
+++ lib-core/src/main/java/com/steamclock/steamock/lib/repo/MockResponse.kt
@@ -2,5 +2,5 @@ package com.steamclock.steamock.lib.repo
 
 sealed class MockResponse {
     class NoneAvailable(val hadError: Exception?): MockResponse()
-    class HasMockUrl(val mockUrl: String): MockResponse()
+    class HasMockUrl(val mockId: String, val mockUrl: String): MockResponse()
 }
