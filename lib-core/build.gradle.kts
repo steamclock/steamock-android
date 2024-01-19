bb5d97d95b5dd585054cb9a4fc5f23adb70c8841
Shayla Sawchenko <shayla@steamclock.com>
2024-01-18 17:49:26 -0800

    Dependency cleanup

--- lib-core/build.gradle.kts
+++ lib-core/build.gradle.kts
@@ -55,62 +55,22 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-
-    // Cannot use 1.10.0 --> https://stackoverflow.com/questions/77271961/dependency-androidx-activityactivity1-8-0-requires-libraries-or-apps-that-de
-    implementation("com.google.android.material:material:1.8.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
     // Compose
     val composeBom = platform("androidx.compose:compose-bom:2023.10.01")
     implementation(composeBom)
     androidTestImplementation(composeBom)
-
-    // Choose one of the following:
-    // Material Design 3
-    //implementation("androidx.compose.material3:material3")
-    // or Material Design 2
     implementation("androidx.compose.material:material")
-    // or skip Material Design and build directly on top of foundational components
-    //implementation("androidx.compose.foundation:foundation")
-    // or only import the main APIs for the underlying toolkit systems,
-    // such as input and measurement/layout
-    //implementation("androidx.compose.ui:ui")
-
-    // Android Studio Preview support
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
 
-    // UI Tests
-    //androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    //debugImplementation("androidx.compose.ui:ui-test-manifest")
-
-    // Optional - Included automatically by material, only add when you need
-    // the icons but not the material library (e.g. when using Material3 or a
-    // custom design system based on Foundation)
-    implementation("androidx.compose.material:material-icons-core")
-    // Optional - Add full set of material icons
-    implementation("androidx.compose.material:material-icons-extended")
-    // Optional - Add window size utils
-    implementation("androidx.compose.material3:material3-window-size-class")
-
-    // Optional - Integration with activities
-    implementation("androidx.activity:activity-compose:1.8.1")
-    // Optional - Integration with ViewModels
-    //implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
-    // Optional - Integration with LiveData
-    //implementation("androidx.compose.runtime:runtime-livedata")
-    // Optional - Integration with RxJava
-    //implementation("androidx.compose.runtime:runtime-rxjava2")
-
     // Data serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.1")
 
     // Basic network calls (Postman APIs)
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:okhttp-coroutines:5.0.0-alpha.12")
+
+//    testImplementation("junit:junit:4.13.2")
+//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- lib-retrofit/build.gradle.kts
+++ lib-retrofit/build.gradle.kts
@@ -31,16 +31,7 @@ android {
 
 dependencies {
     implementation(project(":lib-core"))
-
     // Networking (retrofit)
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    //implementation "com.squareup.okhttp3:logging-interceptor:$okhttpLogging"
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
