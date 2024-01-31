51235fc1504a66b50019199cffb588c28dd153ed
Shayla Sawchenko <shayla@steamclock.com>
2024-01-30 16:18:58 -0800

    Added top level maven-publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.android.application") version "8.1.1" apply false
     id("com.android.library") version "8.1.1" apply false
     id("org.jetbrains.kotlin.jvm") version "1.9.0"
-    //`maven-publish`
+    `maven-publish`
 }
 
 subprojects {
